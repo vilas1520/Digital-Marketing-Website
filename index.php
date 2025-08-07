500fa39b7dd361c64b168f8c996cbdb3d97c21c1
Vilas Ulhas Rawale <vilasrawale0@gmail.com>
2025-08-08 01:38:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
-    $con = mysqli_connect('localhost', 'root', '','digital');
+$Name = $_POST['Name'] ?? '';
+$Email = $_POST['Email'] ?? '';
+$Mobile = $_POST['Mobile'] ?? '';
+$Website = $_POST['Website'] ?? '';
 
-    // get the post records
-    $Name = $_POST['Name'];
-    $Email = $_POST['Email'];
-    $Mobile = $_POST['Mobile'];
-    $Website = $_POST['Website'];
-    
-    // database insert SQL code
-    $sql = "INSERT INTO `userdata` (`Id`, `Fname`, `Femail`, `Fmobile`, `Fwebsite`) VALUES ('0', '$Name', '$Email', '$Mobile', '$Website')";
-    
-    // insert in database 
-    $rs = mysqli_query($con, $sql);
-    
-    if($rs)
-    {
-        echo "Contact Records Inserted";
-    }
-    header('location:index.html');
+$conn = mysqli_connect("localhost", "root", "", "digital");
+
+$sql = "INSERT INTO `userdata` (`Name`, `Email`, `Mobile`, `Website`) VALUES ('$Name','$Email','$Mobile','$Website')";
+
+mysqli_query($conn, $sql);
 ?>
